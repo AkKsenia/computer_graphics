0d74403b6163a4ec699014034a65f093bb086f60
Aksyuk Ksenia <102035034+AkKsenia@users.noreply.github.com>
2024-11-13 15:49:26 +0300

    Add files via upload

--- homework/кг_ДЗ/кг_ДЗ.cpp
+++ homework/кг_ДЗ/кг_ДЗ.cpp
@@ -11,6 +11,7 @@ int main() {
     // __TASK 1__
 
     // смайлик
+    /*
     circle(img, Point(200, 200), 100, Scalar(132, 234, 253), -1);
     circle(img, Point(160, 180), 20, Scalar(102, 102, 102), -1);
     circle(img, Point(240, 180), 20, Scalar(102, 102, 102), -1);
@@ -21,11 +22,22 @@ int main() {
     int x = 200;
     int y = 200;
 
+    ellipse(img, Point(200, 200), Size(100, 100), 0, 0, 360, Scalar(102, 102, 102), 2);
+    ellipse(img, Point(160, 180), Size(20, 20), 0, 0, 360, Scalar(102, 102, 102), 2);
+    ellipse(img, Point(240, 180), Size(20, 20), 0, 0, 360, Scalar(102, 102, 102), 2);
+    ellipse(img, Point(200, 220), Size(55, 30), 0, 0, 180, Scalar(102, 102, 102), 2);
+
+    int oldColor = 0xFFFFFF;
+    int newColor = 0xD9D2E9;
+    int x = 200;
+    int y = 200;
+
     int currentColor = GetPixel(img, x, y);
 
     if (currentColor == oldColor) {
         floodFill8(img, x, y, newColor, oldColor);
     }
+    */
 
     // __TASK 2__
 
@@ -46,10 +58,10 @@ int main() {
     CompositeCatmullRom(img, controlPoints);
     */
 
-    imshow("Flood Fill", img);
+    //imshow("Flood Fill", img);
     //imshow("Composite Catmull-Rom curve", img);
-    waitKey(0);
-    imwrite("Flood_Fill.png", img);
+    //waitKey(0);
+    //imwrite("Flood_Fill.png", img);
     //imwrite("Composite_CatmullRom_curve.png", img);
     return 0;
 }
